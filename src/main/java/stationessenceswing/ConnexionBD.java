32a267a0698b60de1db5e08151046039a5ee4b84
sarobidysolphi <sarobidy600@gmail.com>
2026-08-19 12:18:59 +0300

    Fix: mot de passe MySQL = sarobidy

--- src/main/java/stationessenceswing/ConnexionBD.java
+++ src/main/java/stationessenceswing/ConnexionBD.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class ConnexionBD {
     private static final String URL = "jdbc:mysql://localhost:3306/station_essence?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
     private static final String USER = "root";
-    private static final String PASSWORD = "solphi";
+    private static final String PASSWORD = "sarobidy";
 
     static {
         try {
